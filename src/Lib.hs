2e77a3e4c531b97864697d63cf63d7629719d3fc
Edoardo Z <edovi@icloud.com>
2018-09-20 22:15:40 +0200

    Adding FSM logic and Parser to parse commands; fixed parse tests after changing signature

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,17 +1,41 @@
+
 module Lib where
 
+
+type Input = String
+
+-- | Possible state of the deterministic Parser finite automata.
+data ParserState = WaitingOption | WaitingValue | End deriving (Eq, Show)
+
+-- | Product data type representing a parser taking a string as input. 
+-- It is implemented as a finite automata so
+--it also contains information about its current state.
+data Parser = Parser ParserState deriving (Eq, Show)
+
+-- | Option that can be parsed and used to construct a command. Contains two field:
+-- /option/, which is the string symbol used as identifier, and its /value/, another string
+-- (possibily Nothing).
 data Option = Option { option :: String
-                     , value :: String
+                     , value :: Maybe String
                      }
                      deriving (Eq, Show)
 
+-- | A command constructed by Options.
 newtype Command = Command [Option] deriving (Eq, Show) 
+
+transition :: String -> Parser -> Parser
+transition ('-':xs) (Parser WaitingOption)
+  | isValidString xs = Parser WaitingValue
+  | otherwise = Parser WaitingOption
+transition _ (Parser WaitingValue) = Parser WaitingOption
+transition _ (Parser WaitingOption) = Parser WaitingOption
+-- TODO adds remaining matches
 
 validOptions :: [String]
 validOptions = ["d", "D", "u", "rm", "l"]
 
 isValidOption :: Option -> Bool
-isValidOption x = (option x) `elem` validOptions
+isValidOption x = option x `elem` validOptions
 
 isValidString :: String -> Bool
 isValidString s = s `elem` validOptions
@@ -25,7 +49,7 @@ parse s = let y  = words s
               getO ["",_] = []
               getO (x:w:xs) = case first == '-' && isValidString second && head w /= '-' of
                                 True  -> [Option {
-                                    option = second, value = w
+                                    option = second, value = Just w
                                     }] ++ getO xs
                                 False -> getO (w:xs)
                                 where first = head x
--- test/Spec.hs
+++ test/Spec.hs
@@ -5,10 +5,10 @@ import Test.Hspec
 main = hspec $ do
     describe "Command parses works ok" $ do
         it "Parses commands correctly" $ do
-          (parse "-D mtg -d cute -u edo -w whatever") `shouldBe` [Option "D" "mtg", Option "d" "cute", Option "u" "edo"]
-          (parse "-D mtg -d cute -r --t -u edo -w whatever") `shouldBe` [Option "D" "mtg", Option "d" "cute", Option "u" "edo"]
+          (parse "-D mtg -d cute -u edo -w whatever") `shouldBe` [Option "D" (Just "mtg"), Option "d" (Just "cute"), Option "u" (Just "edo")]
+          (parse "-D mtg -d cute -r --t -u edo -w whatever") `shouldBe` [Option "D" (Just "mtg"), Option "d" (Just "cute"), Option "u" (Just "edo")]
           (parse "-D") `shouldBe` []
-          (parse "-D -d cute") `shouldBe` [Option "d" "cute"]
-          (parse "-n -D mtg") `shouldBe` [Option "D" "mtg"]
-          (parse "-n cc -D mtg") `shouldBe` [Option "D" "mtg"]
+          (parse "-D -d cute") `shouldBe` [Option "d" (Just "cute")]
+          (parse "-n -D mtg") `shouldBe` [Option "D" (Just "mtg")]
+          (parse "-n cc -D mtg") `shouldBe` [Option "D" (Just "mtg")]
           (parse "") `shouldBe` []
