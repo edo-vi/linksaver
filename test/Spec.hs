7ffc0d1aa821a2e6ccef47e4eb7b0e04def026a7
Edoardo Z <edovi@icloud.com>
2018-09-22 18:18:36 +0200

    Fixes tests due to changing parsing logic

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,8 +7,8 @@ main = hspec $ do
         it "Parses commands correctly" $ do
           (parse "-D mtg -d cute -u edo -w whatever") `shouldBe` [Option "D" (Just "mtg"), Option "d" (Just "cute"), Option "u" (Just "edo")]
           (parse "-D mtg -d cute -r --t -u edo -w whatever") `shouldBe` [Option "D" (Just "mtg"), Option "d" (Just "cute"), Option "u" (Just "edo")]
-          (parse "-D") `shouldBe` []
-          (parse "-D -d cute") `shouldBe` [Option "d" (Just "cute")]
+          (parse "-D") `shouldBe` [Option "D" Nothing]
+          (parse "-D -d cute") `shouldBe` [Option "D" Nothing, Option "d" (Just "cute")]
           (parse "-n -D mtg") `shouldBe` [Option "D" (Just "mtg")]
           (parse "-n cc -D mtg") `shouldBe` [Option "D" (Just "mtg")]
           (parse "") `shouldBe` []
