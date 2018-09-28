ffb5c85f1219402d914929ca22d4b2a21e9fb743
Edoardo Z <edovi@icloud.com>
2018-09-28 21:12:30 +0200

    Adds rudimentary working main function, fetching all link in local db

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,45 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 import Command
+import Data.String
 import System.Environment (getArgs)
 import System.Hclip(getClipboard)
 import Fill(defaultOptions)
 import Builder(buildCommand)
+import Database.PostgreSQL.Simple
+import Database.PostgreSQL.Simple.FromRow
+import Database.PostgreSQL.Simple.Types
 
+{- 
 main :: IO ()
 main = do
     getArgs >>= -- IO [string] containing the options
         (\options -> getClipboard >>=
             \clip -> print $ buildCommand (Option "l" (Just clip) : defaultOptions) (parseList options))
+-}
+
+main :: IO ()
+main = do
+    options <- getArgs -- IO [string] containing the options
+    clip <- getClipboard
+    conn <- connect $ ConnectInfo "localhost" 5432 "postgres" "" "mtg" 
+    i <- query_ conn "select * from creatures" -- [Link]
+    -- print $ buildCommand (Option "l" (Just clip) : defaultOptions) (parseList options)
+    printLinks $ (i :: [Link])
+
+printLinks :: [Link] -> IO ()
+printLinks [] = return ()
+printLinks (x:xs) = putStr ("id:   ") >> print(_id x)
+                    >> putStr ("link: ") >> putStrLn (link x) 
+                       >> putStr ("desc: ") >> putStrLn (description x)
+                          >> putStrLn (" ") >> printLinks xs
+
+data Link = Link {
+    _id :: Integer,
+    link :: [Char],
+    description :: [Char]
+} deriving (Eq, Show)
+
+instance FromRow Link where
+    fromRow = Link <$> field <*> field <*> field
